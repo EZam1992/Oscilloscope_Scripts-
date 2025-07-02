acbefb2fb7a845927d29bec6fa9262209a100801
erick <ezamora@soeffects.com>
2025-07-01 17:26:24 -0700

    added scrip for the screenshot functionality

---
+++ screenshot.py
@@ -0,0 +1,22 @@
+#import pyvisa
+from Oscilloscope import Oscilloscope
+import sys
+import os
+from datetime import datetime
+from PIL import Image, ImageFont, ImageDraw, ImageOps
+
+def main(args): 
+
+    scope = Oscilloscope()
+
+    while True: # the plan is to run the script, set the trigger parameters, save screenshot 
+        # save csv, and reset 
+        #scope.set_trigger(volts_div, time_div)
+        scope.screenshot(args) 
+        #scope.save_csv() 
+    
+    
+
+
+if __name__ == '__main__': 
+    main(sys.argv)
