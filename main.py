9439f4c54a24d0bbc8fc7e0b1263e16ea8325f59
erick <ezamora@soeffects.com>
2025-07-03 15:04:30 -0700

    added comments thorughout the codebase

--- main.py
+++ main.py
@@ -2,13 +2,23 @@ from Oscilloscope import Oscilloscope
 import sys 
 
 def main(args): 
-    volts_div = 2  # volts
-    time_div = 0.5 # seconds
-    channel_list = [1, 2, 3, 4]
+
+    """ Defines the primary functionality of the script
+    
+    Args: 
+        args (str): any argument the user would like to pass to the script 
+
+    Outputs: csv files and screenshot of oscilloscop capture 
+    
+    """
+    volts_div = int(input("Volts per division (V): ")) # volts
+    time_div = float(input("Specify time per division (s): ")) # seconds
+    channel_list = [1, 2, 3, 4] # Desired channels 
+
 
     scope = Oscilloscope(time_div)
     scope.set_trigger(volts_div, time_div, channel_list)
-    scope.save_csv(volts_div, time_div, channel_list)
+    scope.save_csv(volts_div, time_div, channel_list, args[1])
 
 if __name__ == '__main__': 
     main(sys.argv)
--- oscilloscope.py
+++ oscilloscope.py
@@ -7,7 +7,17 @@ import os
 import numpy as np
 
 class Oscilloscope(): 
-    def __init__(self, time_div): 
+
+    """ Defines an oscilloscope class and common methods"""
+
+    def __init__(self, time_div: int) -> None: 
+
+        """ Establishes connection to the instruments through the PyVisa Driver 
+
+        Args: 
+            time_div (int): argument to set the time division for the oscilloscope
+        
+        """
         self.rm = pyvisa.ResourceManager() 
         for connected_device in self.rm.list_resources(): 
             try: 
@@ -17,22 +27,23 @@ class Oscilloscope():
                 self.response = self.connection.query("*IDN?").lower() 
                 if "rigol" in self.response: 
                     self.type = "Rigol"
-                    print(f"{self.type} Oscilloscope Connected")
             except: 
                 raise IOError("Unable to find an acceptable scope.")
             
             self.set_time_div(time_div)
             
-    def screenshot(self, folder_path):
-         
-        # try: 
-        #     self.fname_stem = str(sys.argv[1])
-        # except IndexError: 
-        #     self.fname_stem = input("Please enter a file name: ")
-        self.fname_stem = "TEST"
+    def screenshot(self, folder_path: str, args: str) -> Image.Image:
 
+        """ Defines functionality to take a screenshot of the oscilloscope 
+
+        Args: 
+            folder_path (str): path of the folder where the screenshot will be stored   
+
+        Outputs: .PNG of the oscilloscope screen 
+        """
+        self.fname_stem = args
         self.date_string = datetime.strftime(datetime.now(), "%Y%m%d_%H%M%S_")
-        self.fname = self.date_string + self.fname_stem + f".png" 
+        self.fname = self.date_string + self.fname_stem + ".png" 
         self.full_path = os.path.join(folder_path, self.fname)
         self.date_stamp_height_px = 23
         self.date_stamp_string = datetime.now().strftime("%A, %d %b %Y %H:%M:%S")
@@ -47,7 +58,6 @@ class Oscilloscope():
         with open(self.full_path, "wb") as self.fout: 
             self.fout.write(self.image_data)
 
-
         self.img = Image.open(self.full_path)
         self.img = ImageOps.expand(self.img,border=self.date_stamp_height_px,fill=0)
         self.img = self.img.crop((self.date_stamp_height_px,
@@ -61,15 +71,22 @@ class Oscilloscope():
                         self.date_stamp_font)
         self.img.save(self.full_path)
 
-        
-
     def set_time_div(self, time_div): 
+        """
+        
+        
+        """
         self.connection.write(f"TIMebase:MAIN:SCALe {time_div}") 
 
     def set_voltage_div(self, channel, volts_div): 
+        """
+        """
         self.connection.write(f":CHANnel{channel}:SCALe {volts_div}")
 
     def set_trigger(self, volts_div, time_div, channel_list):
+        """
+        
+        """
         self.connection.write(":RUN")
         self.trigger_ON = True
         self.connection.write(":TRIGger:MODE EDGE")
@@ -88,6 +105,10 @@ class Oscilloscope():
                 self.set_voltage_div(channel, volts_div)
 
     def configure_channel(self,channel): 
+
+        """
+        
+        """
         self.connection.write(":STOP")
         self.connection.write(f":WAVeform:SOURce CHANnel{channel}")
         self.connection.write(":WAVeform:MODE NORMal")
@@ -96,12 +117,16 @@ class Oscilloscope():
 
     
     def read_ascii(self):
+        """
+        
+        """
         self.connection.write(":WAVeform:DATA?")
         self.data = self.connection.read_raw()
         # Get preamble
         self.preamble = self.connection.query(":WAVeform:PREamble?").split(',')
         self.x_incr = float(self.preamble[4])   # Time between points
         self.x_origin = float(self.preamble[5]) # Time at index 0
+        
         # Parse SCPI header; this is needed bacause the ASCII data 
         # returned by the oscilloscope includes a header when the data is queried: #9<length><data> 
         if self.data[0:1] == b'#':
@@ -113,7 +138,12 @@ class Oscilloscope():
              raise ValueError("Invalid data format from scope.")
         
 
-    def save_csv(self, volts_div, time_div, channel_list):
+    def save_csv(self, volts_div, time_div, channel_list, args: str):
+
+        """
+        
+        
+        """
     
         
 
@@ -149,7 +179,7 @@ class Oscilloscope():
 
                         #writer.writerows(zip(self.times, self.voltages))
                     
-                self.screenshot(folder_path)
+                self.screenshot(folder_path, args)
                     
                 self.set_trigger(volts_div, time_div, channel_list)
 
